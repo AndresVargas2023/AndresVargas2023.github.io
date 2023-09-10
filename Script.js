9ac60c16113dfe4cdfca56b3743044d35caa8839
Andres Vargas <144184578+AndresVargas2023@users.noreply.github.com>
2023-09-10 17:42:33 -0400

    Cambio 3
    Le agregué Javascipt para que tenga funcionalidad. 
    Hice modificaciones en el estilo.
    Y, cambié el logo agregandole el del ministerio de salud.

---
+++ Script.js
@@ -0,0 +1,15 @@
+//console.log("Hola Mundo");
+let miVariable = "Hola";
+console.log(miVariable)
+let hola =123;
+let hi= "mundo ";
+const MI_CONSTANTE = 123;
+console.log(MI_CONSTANTE+hola)
+console.log(miVariable,hi)
+let N1= "6";
+let N2= "6";
+let N3= "6";
+console.log(N2 > N3)
+
+if(N1<N2) && ()
+    
